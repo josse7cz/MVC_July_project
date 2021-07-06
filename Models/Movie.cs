f546561e551a7ce75833288f28fe481a5970aef9
Josef Janda <josefjanda@email.cz>
2021-07-06 16:23:05 +0200

    ok

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -10,14 +10,26 @@ namespace MVC_July_project.Models
     public class Movie
     {
         public int Id { get; set; }
+        
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; }
 
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+       
+        [RegularExpression(@"^[A-Ž]+[a-žA-Ž\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; }
+
+
+        [Range(1, 1000)]
+       // [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+       
         public string Rating { get; set; }
     }
 }
